aa16da8503357c87d747938b7fae9be46621e939
MirmoChang <mirmo0629@gmail.com>
2022-02-24 18:41:42 +0800

    設定JS

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const body = document.querySelector('body');
-const drawer = document.querySelector('.drawer-open');
+const drawer = document.querySelector('.drawer-nav-open');
 const drawerNav = document.querySelector('.drawer-nav');
 
-window.addEventListener("click",function(){
-    if(document.querySelector('.drawer-nav').style.left = 0){
-        document.querySelector('.drawer-nav').remove('drawer-nav--open');
-    }else{
-        document.querySelector('.drawer-nav').add('drawer-nav--open');
-    }
+window.addEventListener("click", function () {
+    // document.getElementsByClassName('drawer-nav')[0].id = 'drawer-nav-show';
+    document.getElementsByClassName('drawer-nav')[0].style.left = '0';
+    // drawerNav.classList.toggle('.drawer-nav-open');
 })
