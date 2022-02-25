bbb25dad384f1cd0aaf2220eef01eef6f8847dd3
ChiehYu Chang <chiehyuchang@ChiehYude-MacBook-Pro.local>
2022-02-25 18:50:56 +0800

    手機左側彈窗

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const body = document.querySelector('body');
 const drawer = document.querySelector('.drawer-nav-open');
-const drawerNav = document.querySelector('.drawer-nav');
+const overLay = document.querySelector('.drawer-overlay');
 
-window.addEventListener("click", function () {
-    // document.getElementsByClassName('drawer-nav')[0].id = 'drawer-nav-show';
-    document.getElementsByClassName('drawer-nav')[0].style.left = '0';
-    // drawerNav.classList.toggle('.drawer-nav-open');
+drawer.addEventListener("click", function(){
+    document.getElementsByClassName('drawer-nav')[0].id = 'drawer-nav-show';
+    document.getElementsByClassName('drawer-overlay')[0].id = 'draw-overlay';
 })
+
+overLay.addEventListener("click", function(){
+    document.getElementById('drawer-nav-show').removeAttribute('id');
+    document.getElementById('draw-overlay').removeAttribute('id');
+})
+
