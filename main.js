84840bc3eee457cc1b10e3e692821b543c11d2c2
MirmoChang <mirmo0629@gmail.com>
2022-03-24 14:08:49 +0800

    增加輪播圖

--- main.js
+++ main.js
@@ -11,3 +11,41 @@ overLay.addEventListener("click", function(){
     document.getElementById('draw-overlay').removeAttribute('id');
 })
 
+
+//輪播圖的JS
+$(".responsive").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: {
+        delay: 5000,
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  
